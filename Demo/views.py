a991c144b324168b5708ee8473ad273e9210dfa2
Oliver Chu <china90cc@gmail.com>
2017-08-10 10:35:40 +0800

    Initial commit

---
+++ Demo/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from  django.http import HttpResponse
+
+
+# Create your views here.
+def hello(request):
+    student = {
+        'name': 'Oliver Chu',
+        'age': '24'
+    }
+    body = render(request, 'hello.html', {'hello': student})
+    # body= 'sadasd'
+    return HttpResponse(body)
